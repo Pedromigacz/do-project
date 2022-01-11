235d7c8c1191cd95baaeb5a7856c420f8fd9e45d
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-10 23:45:06 -0300

    quickstart typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
