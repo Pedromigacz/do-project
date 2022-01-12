8fa1f5209991b007830db8f8e7c9067ad4bd912f
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-12 11:31:13 -0300

    add InformationWrapper

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import styled from "styled-components"
 
 import { graphql, useStaticQuery } from "gatsby"
-import { getImage, GatsbyImage } from "gatsby-plugin-image"
+import { getImage } from "gatsby-plugin-image"
 
 import { convertToBgImage } from "gbimage-bridge"
 import BackgroundImage from "gatsby-background-image"
@@ -28,23 +28,73 @@ const HomePageLayout: React.FC = () => {
   const bgImage = convertToBgImage(getImage(filteredBackgroundImage))
 
   return (
-    <Layout>
-      <BackgroundImage
-        Tag="section"
-        // Spread bgImage into BackgroundImage:
-        {...bgImage}
-        preserveStackingContext
-        style={{ height: "100vh" }}
-      >
-        HomePageLayout
-      </BackgroundImage>
-    </Layout>
+    <BackgroundImage
+      Tag="section"
+      // Spread bgImage into BackgroundImage:
+      {...bgImage}
+      preserveStackingContext
+      style={{
+        height: "100vh",
+      }}
+    >
+      <Layout>
+        {/* <Img fixed={data.file.childImageSharp.fixed} /> */}
+        <InformationWrapper>
+          <MainTitle>Seed Chumbi</MainTitle>
+          <SecondTitle>Sale is closed 0/1000</SecondTitle>
+          <ThirdTitle>The pods will hatch soon!</ThirdTitle>
+          <Paragraph>
+            Seed Chumbi will be the first and rarest Chumbi NFTs to ever exist!
+            if you missed the sale, you can still buy them on opensea.
+          </Paragraph>
+        </InformationWrapper>
+      </Layout>
+    </BackgroundImage>
   )
 }
 
 const Layout = styled.div`
   width: 100%;
   min-height: 100vh;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+`
+
+const InformationWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+`
+
+const MainTitle = styled.h1`
+  font-family: var(--main-font);
+  font-weight: 900;
+  color: #2caadf;
+  font-size: 4.5rem;
+  margin: 0;
+`
+
+const SecondTitle = styled.h2`
+  font-family: var(--main-font);
+  font-weight: 700;
+  color: #c14f67;
+  font-size: 4rem;
+  margin: 0;
+`
+
+const ThirdTitle = styled.h3`
+  font-family: var(--second-font);
+  font-weight: 700;
+  color: #2caadf;
+  font-size: 28px;
+  margin: 0;
+`
+
+const Paragraph = styled.p`
+  font-family: var(--second-font);
+  font-weight: 600;
+  color: #ffffff;
+  font-size: 22px;
+  margin: 0;
 `
 
 export default HomePageLayout
