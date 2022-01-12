112867f73a81a079a3b00ba4387d7a71be56d5f2
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-12 20:25:55 -0300

    add left padding

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -74,6 +74,7 @@ const Layout = styled.div`
   min-height: 100vh;
   height: 100vh;
   display: grid;
+  padding-left: 4rem;
   grid-template-columns: 2fr 3fr;
   grid-template-rows: 1fr 3fr;
   gap: 10px;
